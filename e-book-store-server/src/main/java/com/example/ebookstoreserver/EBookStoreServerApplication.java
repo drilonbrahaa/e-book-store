6981b55a934dd9d5f3d3e4937b7da467e5fd703f
drilonbrahaa <drilonbraha@gmail.com>
2026-01-20 15:05:23 +0100

    initialized app

---
+++ e-book-store-server/src/main/java/com/example/ebookstoreserver/EBookStoreServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ebookstoreserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBookStoreServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBookStoreServerApplication.class, args);
+    }
+
+}
