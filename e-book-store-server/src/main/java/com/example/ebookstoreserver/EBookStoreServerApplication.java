cd7e2437f43d8cb79d88fc2c7da319bc97d47f44
drilonbrahaa <drilonbraha@gmail.com>
2026-01-22 13:20:32 +0100

    removed 'public' from main method

--- e-book-store-server/src/main/java/com/example/ebookstoreserver/EBookStoreServerApplication.java
+++ e-book-store-server/src/main/java/com/example/ebookstoreserver/EBookStoreServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EBookStoreServerApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(EBookStoreServerApplication.class, args);
     }
 
